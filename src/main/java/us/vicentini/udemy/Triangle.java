e123643f3da1ad020c1fa8054dda756f3068d9d3
Henrique Vicentini <shulander@gmail.com>
2018-05-06 19:06:31 -0300

    Lecture 9.  Bean Autowiring
    Section 1, Lecture 9

--- src/main/java/us/vicentini/udemy/Triangle.java
+++ src/main/java/us/vicentini/udemy/Triangle.java
@@ -1,6 +1,5 @@
 package us.vicentini.udemy;
 
-import java.util.List;
 import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,11 +12,13 @@ import lombok.extern.slf4j.Slf4j;
 @ToString
 public class Triangle {
 
-    private List<Point> points;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
     public void draw() {
-        for (Point point : points) {
-            log.info("Point: " + Objects.toString(point));
-        }
+        log.info("Point A: " + Objects.toString(pointA));
+        log.info("Point B: " + Objects.toString(pointB));
+        log.info("Point C: " + Objects.toString(pointC));
     }
 }
