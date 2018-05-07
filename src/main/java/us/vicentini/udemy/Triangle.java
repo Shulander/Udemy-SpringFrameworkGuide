7b67397861e2846866201dd339c30d72e816fceb
Henrique Vicentini <shulander@gmail.com>
2018-05-06 23:03:49 -0300

    Lecture 13. Lifecycle Callbacks using both (impl interface and context)
    Section 1, Lecture 13
    * Implementing interfaces:
    ** InitializingBean
    ** DisposableBean
    * Using spring context configuration with default

--- src/main/java/us/vicentini/udemy/Triangle.java
+++ src/main/java/us/vicentini/udemy/Triangle.java
@@ -6,11 +6,13 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
 @Getter
 @Slf4j
 @ToString
-public class Triangle implements BeanNameAware {
+public class Triangle implements BeanNameAware, InitializingBean, DisposableBean {
 
     @Setter
     private Point pointA;
@@ -40,5 +42,15 @@ public class Triangle implements BeanNameAware {
 
     public void cleanUp() throws Exception {
         log.info(String.format("My Destroying Bean: destroy method called for Triangle(%s)", beanName));
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        log.info(String.format("Destroying Bean: destroy method called for Triangle(%s)", beanName));
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info(String.format("Initializing Bean: init method called for Triangle(%s)", beanName));
     }
 }
