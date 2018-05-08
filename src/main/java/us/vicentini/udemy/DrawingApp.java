6c8798d347c33e4c8f2c52b3ad5f7b85cc2cab38
Henrique Vicentini <shulander@gmail.com>
2018-05-08 00:00:34 -0300

    Lecture 19. Some JSR-250 Annotations
    Section 1, Lecture 19

--- src/main/java/us/vicentini/udemy/DrawingApp.java
+++ src/main/java/us/vicentini/udemy/DrawingApp.java
@@ -1,16 +1,16 @@
 package us.vicentini.udemy;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import us.vicentini.udemy.shape.Shape;
 
 class DrawingApp {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        context.registerShutdownHook();
 
         Shape shape = context.getBean("circle", Shape.class);
         shape.draw();
     }
-
 }
--- src/main/java/us/vicentini/udemy/shape/Circle.java
+++ src/main/java/us/vicentini/udemy/shape/Circle.java
@@ -1,24 +1,34 @@
 package us.vicentini.udemy.shape;
 
 import java.util.Objects;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 @Slf4j
 public class Circle implements Shape {
 
     @Getter
     @Setter
-    @Autowired
-    @Qualifier("centerRelated")
+    @Resource(name = "pointA")
     private Point center;
 
     @Override
     public void draw() {
         log.info("Drawing a Circle");
         log.info("Circle: Center Point is: " + Objects.toString(center));
+    }
+
+    @PostConstruct
+    public void initCircle() {
+        log.info("Initializing using annotation");
+    }
+
+    @PreDestroy
+    public void destroyCircle() {
+        log.info("Destroying using annotation");
     }
 }
