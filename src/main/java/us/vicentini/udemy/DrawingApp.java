0239d26262301b9834cc0436e65ee5aa75ddcf31
Henrique Vicentini <shulander@gmail.com>
2018-05-08 00:28:48 -0300

    Lecture 21. Using MessageSource To Get Text From Property Files
    Section 1, Lecture 21

--- src/main/java/us/vicentini/udemy/DrawingApp.java
+++ src/main/java/us/vicentini/udemy/DrawingApp.java
@@ -1,9 +1,11 @@
 package us.vicentini.udemy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import us.vicentini.udemy.shape.Shape;
 
+@Slf4j
 class DrawingApp {
 
     public static void main(String[] args) {
@@ -13,5 +15,4 @@ class DrawingApp {
         Shape shape = context.getBean("circle", Shape.class);
         shape.draw();
     }
-
 }
--- src/main/java/us/vicentini/udemy/shape/Circle.java
+++ src/main/java/us/vicentini/udemy/shape/Circle.java
@@ -1,26 +1,28 @@
 package us.vicentini.udemy.shape;
 
+import java.util.Locale;
 import java.util.Objects;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class Circle implements Shape {
 
-    @Getter
-    @Setter
     @Autowired
     @Qualifier("pointA")
     private Point center;
 
+    @Autowired
+    private MessageSource messageSource;
+
     @Override
     public void draw() {
-        log.info("Drawing a Circle");
-        log.info("Circle: Center Point is: " + Objects.toString(center));
+        log.info(messageSource.getMessage("circle.drawing", null, "circle.drawing", Locale.ENGLISH));
+        log.info(messageSource
+            .getMessage("circle.center", new Object[]{Objects.toString(center)}, "circle.center", Locale.ENGLISH));
     }
 }
