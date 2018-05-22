08863b0d13560158b1a9dc8fe1997e6e2974e305
Henrique Vicentini <shulander@gmail.com>
2018-05-21 22:32:43 -0400

    S08 Lecture 27.  Pointcuts and Wildcard Expressions
    Section 8, Lecture 27

--- src/main/java/us/vicentini/udemy/aspect/LoggingAspect.java
+++ src/main/java/us/vicentini/udemy/aspect/LoggingAspect.java
@@ -3,13 +3,27 @@ package us.vicentini.udemy.aspect;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Slf4j
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(public String getName())")
+    @Before("execution(public String us.vicentini.udemy.shape.Circle.getName())")
     public void loggingAspect() {
         log.info("Advice run. Get Method Called");
     }
+
+    @Before("allGetters()")
+    public void wildCardAdvice() {
+        log.info("Advice run. Using wildcard (1)");
+    }
+
+    @Before("allGetters()")
+    public void secondAdviceWithPointcut() {
+        log.info("Advice run. Using wildcard (2)");
+    }
+
+    @Pointcut("execution(public * get*())")
+    public void allGetters(){}
 }
